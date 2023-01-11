6cbeb998adf000436632a231fa4298607ec3dfcf
wenblack <wenblack012@gmail.com>
2023-01-11 13:18:25 -0300

    Create folders,and add initial plugins

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  Inter_900Black,
+} from '@expo-google-fonts/inter';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default () => {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+
+        <Text style={{ fontFamily: 'Inter_900Black' }}>
+          Inter Black
+        </Text>
+
+      </View>
+    );
+  }
+};
